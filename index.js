c3cd56876639486da71014f63b1befd0ac42e0d3
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-03-04 16:41:14 +0530

    nice

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-require('dotenv').config(); // Load environment variables
+require('dotenv').config(); 
 const express = require('express');
-const { resolve } = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 3010;
+const port = 4000 ;
 const MONGO_URI = process.env.MONGO_URI;
 
-// Serve static files
-app.use(express.static('static'));
 
-// Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
 .then(() => console.log("Connected to database"))
 .catch((error) => console.error("Error connecting to database:", error));
 
-// Serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+    res.json("Well done")
+})
 
-// Start the server
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
